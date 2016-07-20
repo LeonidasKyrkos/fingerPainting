2996b8330be61e014a08c761dc47cb7102d0a1b8
LeonidasKyrkos <leo.kyrkos@gmail.com>
2016-07-20 18:26:13 +0100

    built

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -86,7 +86,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('52.209.86.125:443');
 
 var App = function (_Component) {
 	_inherits(App, _Component);
