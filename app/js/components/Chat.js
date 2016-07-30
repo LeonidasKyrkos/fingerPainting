7e7842e9e3d8ee3205fee11298fc857dd1d7186f
Leo Kyrkos <leo.kyrkos@gmail.com>
2016-07-30 14:29:52 +0100

    added error catch for chat

--- app/js/components/Chat.js
+++ app/js/components/Chat.js
@@ -24,8 +24,10 @@ export default class Chat extends Component {
 		this.setState(state);
 	}
 
-	shouldComponentUpdate(nextProps, nextState) {
-		return Object.keys(this.state.store.chatLog)[Object.keys(this.state.store.chatLog).length - 1] !== Object.keys(nextState.store.chatLog)[Object.keys(nextState.store.chatLog).length - 1];
+	shouldComponentUpdate(nextProps, nextState) {		
+		if(this.state.store.chatLog) {
+			return Object.keys(this.state.store.chatLog)[Object.keys(this.state.store.chatLog).length - 1] !== Object.keys(nextState.store.chatLog)[Object.keys(nextState.store.chatLog).length - 1];
+		}
 	}
 
 	parseChatForm(e) {
