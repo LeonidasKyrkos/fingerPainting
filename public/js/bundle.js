d3f2f4b1f47e90b364f0787674a7bb130fbb47d2
Leo Kyrkos <leo.kyrkos@gmail.com>
2016-11-01 17:09:30 +0000

    changed it back

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1201,7 +1201,7 @@ var App = function (_Component) {
 
 		var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-		_this.socket = io.connect('http://localhost:3000');
+		_this.socket = io.connect('http://fingerpainting.io');
 		return _this;
 	}
 
